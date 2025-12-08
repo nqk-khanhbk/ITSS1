9bc4f716e37f55bf83189fee75c8bea60199ff1f
LyMuc <lethaison19122004@gmail.com>
2025-12-08 17:13:15 +0700

    API/viewPlaceRanking

--- backend/routes/place.routes.js
+++ backend/routes/place.routes.js
@@ -6,11 +6,15 @@ const placeController = require("../controllers/place.controller");
 // URL: /api/places/filters
 router.get("/filters", placeController.getFilterOptions);
 
-// 2. Route tìm kiếm (Static route - Phải đặt TRƯỚC dynamic route)
+// 2. Route tìm kiếm
 // URL: /api/places/search
 router.get("/search", placeController.searchPlaces);
 
-// 3. Route lấy chi tiết (Dynamic route - Đặt SAU)
+// 3. Route bảng xếp hạng
+// URL: /api/places/ranking
+router.get("/ranking", placeController.getPlacesRanking);
+
+// 4. Route lấy chi tiết (Dynamic route - Đặt SAU CÙNG)
 // URL: /api/places/:id
 router.get("/:id", placeController.getPlaceDetail);
 
